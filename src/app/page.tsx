b41ac1606f312d2a935cd9d2396ffefaf79da2a1
Ravil2 <rafffa141105@gmail.com>
2025-07-03 23:24:49 +0500

    Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Actions from '@/components/Actions/Actions';
 import Slider from '@/components/Slider/Slider';
 
 export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider />
+      <Actions />
     </main>
   );
 }
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+
+const ProductCard = () => {
+  return (
+    <div className="flex flex-col justify-between w-40 rounded overflow-hidden bg-white md:w-[224px] xl:w-[272px] align-top p-0 hover:shadow-(--shadow-article) duration-300">
+      <div className="relative w-40 h-40 md:w-[224px] xl:w-[272px]">
+        <Image
+          src="/images/products/img-3.jpeg"
+          alt="Акция"
+          fill
+          className="object-cover"
+          sizes="(max-width: 768px) 160px, (max-width: 1200px) 224px, 272px"
+        />
+        <button className="w-8 h-8 p-2 bg-[#f3f2f1] hover:bg-[#fcd5ba] absolute top-2 right-2 opacity-50 rounded cursor-pointer duration-300">
+          <Image
+            src="/icons-header/icon-heart.svg"
+            alt="В избранное"
+            width={24}
+            height={24}
+            sizes="24px"
+          />
+        </button>
+        <div className="absolute bg-[#ff6633] py-1 px-2 rounded text-white bottom-2.5 left-2.5">
+          -10%
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between p-2 gap-y-2">
+        <div className="flex flex-row justify-between items-end">
+          <div className="flex flex-col gap-x-1">
+            <div className="flex flex-row gap-x-1 text-sm md:text-lg font-bold">
+              <span>199</span>
+              <span>₽</span>
+            </div>
+            <p className="text-[#bfbfbf] text-[8px] md:text-xs">С картой</p>
+          </div>
+          <div className="flex flex-col gap-x-1">
+            <div className="flex flex-row gap-x-1 text-xs md:text-base text-[#606060]">
+              <span>210</span>
+              <span>₽</span>
+            </div>
+            <p className="text-[#bfbfbf] text-[8px] md:text-xs">Обычная</p>
+          </div>
+        </div>
+        <div className="h-13.5 text-xs md:text-base text-[#414141] line-clamp-3 md:line-clamp-2 leading-[1.5]">
+          Это пример описания товара. Здесь может быть до трёх строк текста.
+        </div>
+        <p>Рейтинг 4.8</p>
+        <button className="border border-(--color-primary) hover:text-white hover:bg-[#ff6633] hover:border-transparent active:shadow-(--shadow-button-active) w-full h-10 rounded p-2 justify-center items-center text-(--color-primary) transition-all duration-300 cursor-pointer select-none">
+          В корзину
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
