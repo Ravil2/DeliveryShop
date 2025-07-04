6beeb32c5875fc80d6ecacc3b3c4279af3b68e8a
Ravil2 <rafffa141105@gmail.com>
2025-07-04 22:43:13 +0500

    rating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider />
-      <div className="px-[max(12,calc((100%-1208px)))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+      <div className="px-[max(12,calc((100%-1208px)))] flex flex-col gap-y-20 md:mb-25 xl:mb-30 md:items-center">
         <Actions />
       </div>
     </main>
