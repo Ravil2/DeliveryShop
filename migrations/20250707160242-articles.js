da8bc8f512a631511e52e9cfea0b948640918271
Ravil2 <rafffa141105@gmail.com>
2025-07-07 21:16:52 +0500

    add products

--- migrations/20250707160242-articles.js
+++ migrations/20250707160242-articles.js
@@ -1,12 +1,9 @@
 const articles = require('./ArticleDatabase.json');
-const database = require('./database.json');
 
 module.exports = {
-  async up(db, client) {
+  async up(db) {
     await db.collection('articles').insertMany(articles);
   },
 
-  async down(db, client) {
-    await db.collection('article').insertMany(database);
-  },
+  // async down(db) {},
 };
